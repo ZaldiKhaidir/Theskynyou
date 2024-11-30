c5b35be840e160919918041c16e40a09d4230f10
Shirokamii <zaldikhaidir123@gmail.com>
2024-11-30 10:55:38 +0700

    Update script.js

--- pages/gift2/script.js
+++ pages/gift2/script.js
@@ -3,59 +3,62 @@ let highestZ = 1;
 class Paper {
   constructor(paper) {
     this.paper = paper;
-    this.holdingPaper = false;
+    this.holding = false;
     this.startX = 0;
     this.startY = 0;
-    this.currentX = 0;
-    this.currentY = 0;
+    this.offsetX = 0;
+    this.offsetY = 0;
     this.rotation = Math.random() * 30 - 15;
 
     this.init();
   }
 
   init() {
-    // Event untuk desktop (mouse)
-    this.paper.addEventListener('mousedown', this.startMove.bind(this));
+    // Event untuk desktop
+    this.paper.addEventListener('mousedown', this.start.bind(this));
     document.addEventListener('mousemove', this.move.bind(this));
-    window.addEventListener('mouseup', this.endMove.bind(this));
+    document.addEventListener('mouseup', this.end.bind(this));
 
-    // Event untuk layar sentuh (touch)
-    this.paper.addEventListener('touchstart', this.startMove.bind(this), { passive: false });
+    // Event untuk layar sentuh
+    this.paper.addEventListener('touchstart', this.start.bind(this), { passive: false });
     document.addEventListener('touchmove', this.move.bind(this), { passive: false });
-    window.addEventListener('touchend', this.endMove.bind(this));
+    document.addEventListener('touchend', this.end.bind(this));
   }
 
-  startMove(e) {
+  start(e) {
     e.preventDefault();
 
-    // Tangkap koordinat awal
-    const touch = e.touches ? e.touches[0] : e;
-    this.startX = touch.clientX - this.currentX;
-    this.startY = touch.clientY - this.currentY;
+    const isTouch = e.type === 'touchstart';
+    const clientX = isTouch ? e.touches[0].clientX : e.clientX;
+    const clientY = isTouch ? e.touches[0].clientY : e.clientY;
 
-    this.holdingPaper = true;
+    this.startX = clientX - this.offsetX;
+    this.startY = clientY - this.offsetY;
+    this.holding = true;
 
-    // Atur z-index agar elemen berada di atas
+    // Tingkatkan z-index elemen yang dipegang
     this.paper.style.zIndex = highestZ;
     highestZ++;
   }
 
   move(e) {
-    if (!this.holdingPaper) return;
+    if (!this.holding) return;
 
     e.preventDefault();
 
-    // Tangkap koordinat gerakan
-    const touch = e.touches ? e.touches[0] : e;
-    this.currentX = touch.clientX - this.startX;
-    this.currentY = touch.clientY - this.startY;
+    const isTouch = e.type === 'touchmove';
+    const clientX = isTouch ? e.touches[0].clientX : e.clientX;
+    const clientY = isTouch ? e.touches[0].clientY : e.clientY;
 
-    // Terapkan transformasi pada elemen
-    this.paper.style.transform = `translate(${this.currentX}px, ${this.currentY}px) rotate(${this.rotation}deg)`;
+    this.offsetX = clientX - this.startX;
+    this.offsetY = clientY - this.startY;
+
+    // Terapkan transformasi posisi dan rotasi
+    this.paper.style.transform = `translate(${this.offsetX}px, ${this.offsetY}px) rotate(${this.rotation}deg)`;
   }
 
-  endMove() {
-    this.holdingPaper = false;
+  end() {
+    this.holding = false;
   }
 }
 
